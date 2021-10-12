1d07fb3ce054799ec9d719fe59ba8a4f99bf57cc
thibault <thibault.latrace@gmail.com>
2021-10-12 09:49:54 +0200

    n_neighbors/weight ratio sorting

--- custom_scripts.py
+++ custom_scripts.py
@@ -1,40 +1,91 @@
 import networkx as nx
 from pathlib import Path
-from dominant import load_graph
+from dominant import load_graph, dominant
 import random
 import matplotlib.pyplot as plt
 
-INPUT_DIR = Path(r"C:\Users\thiba\OneDrive - CentraleSupelec\3A_Centrale_Supélec\COURS\ALGO-ALGORITHMIQUE_AVANCEE\concours_algo\public_data\public_dataset")
+INPUT_DIR = Path(
+    r"C:\Users\thiba\OneDrive - CentraleSupelec\3A_Centrale_Supélec\COURS\ALGO-ALGORITHMIQUE_AVANCEE\concours_algo\public_data\public_dataset"
+)
 GRAPH_FILENAME = "graph_50_50"
-GRAPH_FILENAMES_LIST = ["graph_100_100", "graph_100_1000", "graph_100_250", "graph_100_500", "graph_250_1000", "graph_250_250", "graph_250_500", "graph_500_1000", "graph_500_500", "graph_50_1000", "graph_50_250", "graph_50_50", "graph_50_500"]
+GRAPH_FILENAMES_LIST = [
+    "graph_100_100",
+    "graph_100_1000",
+    "graph_100_250",
+    "graph_100_500",
+    "graph_250_1000",
+    "graph_250_250",
+    "graph_250_500",
+    "graph_500_1000",
+    "graph_500_500",
+    "graph_50_1000",
+    "graph_50_250",
+    "graph_50_50",
+    "graph_50_500",
+]
 
 
 def plot_graph(
-        input_dir: Path,
-        graph_filename: str,
+    input_dir: Path,
+    graph_filename: str,
 ):
     graph = load_graph(input_dir / graph_filename)
     plt.title(graph_filename)
-    labels = {n: str(n) + '\n\n' + str(graph.nodes[n]['weight']) for n in graph.nodes}
+    labels = {n: str(n) + "\n\n" + str(graph.nodes[n]["weight"]) for n in graph.nodes}
     nx.draw(graph, with_labels=True, labels=labels, font_weight="bold")
 
 
 def plot_random_graph(
-        input_dir: Path,
-        graph_filenames_list: list,
+    input_dir: Path,
+    graph_filenames_list: list,
 ):
     graph_filename = random.choice(graph_filenames_list)
     plt.title(graph_filename)
-    plot_graph(
-        input_dir=input_dir,
-        graph_filename=graph_filename
+    plot_graph(input_dir=input_dir, graph_filename=graph_filename)
+
+
+def compute_graph_dominant_set_score(
+        input_dir: Path,
+        graph_filename: str,
+        dominant_set: list,
+) -> int:
+    graph = load_graph(input_dir / graph_filename)
+    return sum(
+        [
+            weight_dict["weight"]
+            for node_number, weight_dict in dict(graph.nodes(data=True)).items()
+            if node_number in dominant_set
+        ]
     )
 
 
-def compute_graph_score(
-        graph: nx.Graph,
+def compute_global_score(
+        input_dir: Path,
+        graph_filenames_list: list,
+) -> int:
+    total_score = 0
+    for graph_filename in graph_filenames_list:
+        graph = load_graph(input_dir / graph_filename)
+        dominating_set = dominant(graph)
+        total_score += compute_graph_dominant_set_score(
+            input_dir=input_dir,
+            graph_filename=graph_filename,
+            dominant_set=dominating_set,
+        )
+    return total_score
+
+
+def get_node_weight(node: tuple):
+    return node[1]["weight"]
+
+
+def test_dominant(
+    input_dir: Path,
+    graph_filename: str,
 ):
-    ...
+    graph = load_graph(input_dir / graph_filename)
+    return dominant(graph)
+
 
 
 
@@ -43,3 +94,6 @@ def compute_graph_score(
 
 # plot_graph(INPUT_DIR, GRAPH_FILENAME)
 # plot_random_graph(INPUT_DIR, GRAPH_FILENAMES_LIST)
+# get_node_neighbors(INPUT_DIR, GRAPH_FILENAME, 6)
+# test_dominant(INPUT_DIR, GRAPH_FILENAME)
+# compute_global_score(INPUT_DIR, GRAPH_FILENAMES_LIST)
--- dominant.py
+++ dominant.py
@@ -1,17 +1,65 @@
-import sys, os, time
+import sys
+import os
+import time
 import networkx as nx
+import operator
 
 
 def dominant(graph) -> list:
     """
-        A Faire:
-        - Ecrire une fonction qui retourne le dominant du graphe non dirigé g passé en parametre.
-        - cette fonction doit retourner la liste des noeuds d'un petit dominant de g
+    A Faire:
+    - Ecrire une fonction qui retourne le dominant du graphe non dirigé g passé en parametre.
+    - cette fonction doit retourner la liste des noeuds d'un petit dominant de g
 
-        :param graph: le graphe est donné dans le format networkx : https://networkx.github.io/documentation/stable/reference/classes/graph.html
+    :param graph: le graphe est donné dans le format networkx : https://networkx.github.io/documentation/stable/reference/classes/graph.html
 
     """
-    return list(nx.dominating_set(graph))
+    nodes_dict = {
+        node_number: {
+            "weight": weight_dict["weight"],
+            "neighbors": get_node_neighbors(graph=graph, node_number=node_number),
+            "ratio": len(get_node_neighbors(graph=graph, node_number=node_number)) / weight_dict["weight"]
+        }
+        for node_number, weight_dict in dict(graph.nodes(data=True)).items()
+    }
+
+    nodes_ratio_dict = {
+        node_number: node_dict["ratio"]
+        for node_number, node_dict in nodes_dict.items()
+    }
+
+    sorted_nodes_ratio_list = sorted(nodes_ratio_dict.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_nodes_list = [node_tuple[0] for node_tuple in sorted_nodes_ratio_list]
+
+    dominating_set = list()
+    counter = 0
+    while (not nx.is_dominating_set(graph, dominating_set)) and (counter <= len(list(graph.nodes))):
+        next_node = sorted_nodes_list[counter]
+        if not is_node_linked_to_dominating_set(next_node, graph, dominating_set):
+            dominating_set.append(next_node)
+        counter += 1
+
+    return sorted(dominating_set)
+
+
+def get_node_neighbors(
+    graph: nx.Graph,
+    node_number: int,
+) -> list:
+    return [edge[1] for edge in list(graph.edges(node_number))]
+
+
+def is_node_linked_to_dominating_set(
+        node: int,
+        graph: nx.Graph,
+        dominating_set: list,
+) -> bool:
+    for dominating_node in dominating_set:
+        graph_node_neighbors = get_node_neighbors(graph, dominating_node)
+        if node in graph_node_neighbors:
+            return True
+    return False
+
 
 #########################################
 #### Ne pas modifier le code suivant ####
@@ -79,8 +127,10 @@ if __name__ == "__main__":
         exit()
 
     # fichier des reponses depose dans le output_dir et annote par date/heure
-    output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))
-    output_file = open(os.path.join(output_dir, output_filename), 'w')
+    output_filename = "answers_{}.txt".format(
+        time.strftime("%d%b%Y_%H%M%S", time.localtime())
+    )
+    output_file = open(os.path.join(output_dir, output_filename), "w")
 
     for graph_filename in sorted(os.listdir(input_dir)):
         # importer le graphe
@@ -92,7 +142,7 @@ if __name__ == "__main__":
         # ajout au rapport
         output_file.write(graph_filename)
         for node in D:
-            output_file.write(' {}'.format(node))
-        output_file.write('\n')
+            output_file.write(" {}".format(node))
+        output_file.write("\n")
 
     output_file.close()
